c22d8728676240f444867ab4860f4b5aeb60039e
Lucas Panfieti <158314592+LucasPanfieti@users.noreply.github.com>
2025-07-11 18:19:23 -0300

    Delete aula_2_gpt.py

--- aula_2_gpt.py
+++
@@ -1,9 +0,0 @@
-nome = "Lucas"
-idade = 23
-altura = 1.785
-
-print(type(nome))
-print(type(idade))
-print(type(altura))
-
-print(f"Meu nome é {nome}, tenho {idade} anos e minha altura é {altura:.2f}m")
